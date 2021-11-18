1f56b8b8b5af04eaaa4ccc2b852b5e832faeaa1d
emmaudeji <gogreneinfo@gmail.com>
2021-11-18 13:15:11 -0800

    sum of infinite arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    sum_args = 0
+    n = 0
     for i in range(len(argv) - 1):
-        sum_args += int(argv[i + 1])
+        n += int(argv[i + 1])
     print("{:d}".format(n))
