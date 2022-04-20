1696a7e4ccdc311b7e0a83db9fea80da4119be80
Emmanuel Udeji <88405403+emmaudeji@users.noreply.github.com>
2022-04-20 16:54:31 +0100

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,5 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+$.get('http://swapi.co/api/films?format=json', (data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    $('ul#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
 });
